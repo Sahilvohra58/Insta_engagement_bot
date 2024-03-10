0ebfc491bac96b1d9d4f4a7746bae105da428a1f
Sahilvohra58 <vohrasahil58@gmail.com>
2024-03-10 12:24:49 -0400

    temp change

--- setup.sh
+++ setup.sh
@@ -3,5 +3,10 @@ sudo apt-get update
 sudo apt-get install docker.io
 sudo apt-get install docker-compose
 
-sudo docker build -t like_comments_i3 .
-sudo docker run like_comments_i3
+sudo git clone https://github.com/Sahilvohra58/Insta_engagement_bot.git
+cd Insta_engagement_bot
+sudo docker-compose up -d
+
+# sudo docker build -t like_comments_i3 .
+# sudo docker run like_comments_i3
+# sudo rm -rf Insta_engagement_bot
